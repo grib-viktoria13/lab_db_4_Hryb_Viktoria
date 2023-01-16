6aefe609a81fa9aa8370133c5b91310e2f1805d2
grib-viktoria13 <72021002+grib-viktoria13@users.noreply.github.com>
2023-01-16 19:13:22 +0200

    Add files via upload

--- procedure.sql
+++ procedure.sql
@@ -1,6 +1,6 @@
 --- Створити процедуру get_artist(country_arg), яка надає інформацію про першого артиста з країни, вказаної в аргументі. 
-DROP PROCEDURE IF EXISTS get_artist(varchar(50));
-CREATE OR REPLACE PROCEDURE get_vendors(country_arg varchar(50))
+--DROP PROCEDURE IF EXISTS get_artist(varchar(50));
+CREATE OR REPLACE PROCEDURE get_artists(country_arg varchar(50))
 LANGUAGE 'plpgsql'
 AS $$
 DECLARE record_name artist.name%TYPE;
@@ -10,9 +10,6 @@ DECLARE record_country artist.country%TYPE;
 BEGIN
 SELECT name, country into record_name, record_country FROM artist WHERE country = country_arg;
 	RAISE INFO 'artist_name: %,  artist_country: %', TRIM(record_name), TRIM(record_country);
-
-	
-
 END;
 $$;
 
